648b6612cd3591572a8cc7ad49c4202a276a4591
danmotafs <danmotafscorporativo@gmail.com>
2026-05-05 19:32:57 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "StreetFi Angels",
+  description: "SocialFi for real-world merchants",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
