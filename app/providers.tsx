81a918308db620c1d87651a1efdbe3954cfba8b0
danmotafs <danmotafscorporativo@gmail.com>
2026-05-06 20:30:04 -0300

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -17,17 +17,15 @@ import {
 
 import { clusterApiUrl } from "@solana/web3.js";
 
-require("@solana/wallet-adapter-react-ui/styles.css");
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 export default function Providers({
   children,
 }: {
   children: ReactNode;
 }) {
-  const network = "devnet";
-
   const endpoint = useMemo(() => {
-    return clusterApiUrl(network);
+    return clusterApiUrl("devnet");
   }, []);
 
   const wallets = useMemo(() => {
