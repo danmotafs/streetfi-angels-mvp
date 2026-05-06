6a520689676545f077da9c06084b3acf0c9c6dc9
danmotafs <danmotafscorporativo@gmail.com>
2026-05-06 20:21:42 -0300

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { ReactNode, useMemo } from "react";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import {
+  WalletModalProvider,
+} from "@solana/wallet-adapter-react-ui";
+
+import {
+  PhantomWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import { clusterApiUrl } from "@solana/web3.js";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+export default function Providers({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const network = "devnet";
+
+  const endpoint = useMemo(() => {
+    return clusterApiUrl(network);
+  }, []);
+
+  const wallets = useMemo(() => {
+    return [new PhantomWalletAdapter()];
+  }, []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          {children}
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
